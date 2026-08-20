3aa0f19d960d4ce9ee80b096ad57c941ebd53cd4
unknown <itspratikshah25.1990@gmail.com>
2026-08-20 17:13:09 +0530

    Updated code handled drop down

--- tests/SauceDemoTest.spec.js
+++ tests/SauceDemoTest.spec.js
@@ -15,4 +15,12 @@ test("@Regression End To End Flow", async () =>{
     await page.locator("#login-button").click();
 
     await page.waitForTimeout(7000);
+
+    await page.locator(".product_sort_container").selectOption({label : 'Price (high to low)'});
+
+    await page.waitForTimeout(3000);
+
+    await page.locator("button[name='add-to-cart-sauce-labs-fleece-jacket']").click();
+
+    await page.waitForTimeout(3000);
 });
